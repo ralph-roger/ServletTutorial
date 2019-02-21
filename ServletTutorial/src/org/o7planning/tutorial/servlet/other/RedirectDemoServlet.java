daba86bd9cc2c3a59de764d319aa9a46cb9c23eb
Ralph-Roger <ralph_reifegerste@web.de>
2019-02-21 20:42:10 +0100

    nur imports

--- ServletTutorial/src/org/o7planning/tutorial/servlet/other/RedirectDemoServlet.java
+++ ServletTutorial/src/org/o7planning/tutorial/servlet/other/RedirectDemoServlet.java
@@ -1,16 +1,13 @@
 package org.o7planning.tutorial.servlet.other;
  
 import java.io.IOException;
- 
-import javax.servlet.RequestDispatcher;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
-import org.o7planning.tutorial.beans.Constants;
  
 @WebServlet("/other/redirectDemo")
 public class RedirectDemoServlet extends HttpServlet {
