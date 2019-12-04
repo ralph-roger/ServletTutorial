f3766bef75caf91cbd7dcf9cf14ffa34325c625d
Ralph-Roger <ralph_reifegerste@web.de>
2019-12-04 17:45:54 +0100

    ErsteWebApp init

---
+++ ErsteWebApp/src/meinservletpackage/MeinErstesServlet.java
@@ -0,0 +1,50 @@
+package meinservletpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MeinErstesServlet
+ */
+@WebServlet("/HelloServlet")
+public class MeinErstesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MeinErstesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    response.setContentType( "text/html" );
+	      PrintWriter out = response.getWriter();
+	      out.println( "<html>" );
+	      out.println( "<h3> Hallo, mein erstes Servlet meldet sich </h3>" );
+	      out.println( "<a href='/ErsteWebApp/'>zur&uuml;ck</a>" );
+	      out.println( "</html>" );
+	      out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
